696ee29e89b7e1eff0b8ff307478aad1fa466c49
Yun Jang <jang.yunj7@gmail.com>
2023-01-30 00:17:35 -0500

    initial commit

---
+++ API/Program.cs
@@ -0,0 +1,7 @@
+﻿using Newtonsoft.Json.Linq;
+
+var client = new HttpClient();
+var makeupURL = "https://makeup-api.herokuapp.com/api/v1/products.json?brand=maybelline";
+var makeupResponse = client.GetStringAsync(makeupURL).Result;
+var makeupObject = JArray.Parse(makeupResponse);
+Console.WriteLine($"Maybe it's {makeupObject[0]["brand"]} ;)");
